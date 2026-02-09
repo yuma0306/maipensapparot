cfd821fda9f371981d682bc586d938e1897f7a80
yuma0306 <yuma0306895@gmail.com>
2026-02-10 00:18:28 +0700

    修正

---
+++ src/lib/components/Speakbutton/Speakbutton.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import SoundIcon from '../Icon/SoundIcon.svelte';
+	import type { Phrase } from '$lib/types/Phrase';
+
+	type Props = {
+		text: Phrase['thai'];
+	};
+
+	let { text }: Props = $props();
+
+	function speak(text: string) {
+		const utterance = new SpeechSynthesisUtterance(text);
+		utterance.lang = 'th-TH';
+		speechSynthesis.speak(utterance);
+	}
+</script>
+
+<button onclick={() => speak(text)} class="button">
+	<SoundIcon />
+</button>
+
+<style>
+	.button {
+		display: grid;
+		place-content: center;
+		background-color: var(--color-secondary-10);
+		color: var(--color-secondary);
+		border: 1px solid var(--color-secondary);
+		transition: opacity var(--transition);
+		line-height: 1;
+		&:hover {
+			opacity: var(--opacity);
+		}
+	}
+	@media (min-width: 640px) {
+		.button {
+			cursor: pointer;
+			padding: calc(var(--spacing-1) * 1px);
+			border-radius: calc(var(--border-radius) * 1px);
+		}
+	}
+	@media (max-width: 639px) {
+		.button {
+			border-radius: calc(var(--calc-sp) * var(--border-radius));
+			padding: calc(var(--calc-sp) * var(--spacing-1));
+		}
+	}
+</style>
