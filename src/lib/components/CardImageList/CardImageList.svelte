772013e86f9666b5d5a134abcf35c80411a91787
yuma0306 <yuma0306895@gmail.com>
2026-02-11 07:54:16 +0700

    カード修正

--- src/lib/components/CardImageList/CardImageList.svelte
+++ src/lib/components/CardImageList/CardImageList.svelte
@@ -18,12 +18,12 @@
 		width: 100%;
 		justify-content: center;
 		@media (min-width: 640px) {
-			grid-template-columns: repeat(auto-fit, max(227px));
-			gap: calc(var(--spacing-2) * 1px);
+			grid-template-columns: repeat(auto-fit, max(193px));
+			gap: calc(var(--spacing-1) * 1px);
 		}
 		@media (max-width: 639px) {
-			grid-template-columns: repeat(2, 1fr);
-			gap: calc(var(--calc-sp) * var(--spacing-2));
+			grid-template-columns: repeat(auto-fit, max(calc(var(--calc-sp) * 114)));
+			gap: calc(var(--calc-sp) * var(--spacing-1));
 		}
 	}
 </style>
