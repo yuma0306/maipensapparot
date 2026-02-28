d575652f888e6f8300cc96d804aea514e96ca6e5
yuma0306 <yuma0306895@gmail.com>
2026-03-01 00:02:15 +0700

    スタイル修正

--- src/lib/components/ExamQuestionCard/ExamQuestionCard.svelte
+++ src/lib/components/ExamQuestionCard/ExamQuestionCard.svelte
@@ -2,12 +2,8 @@
 	import type { ExamQuestion } from '$lib/types';
 	import Card from '$lib/components/Card/Card.svelte';
 	import Stack from '$lib/components/Stack/Stack.svelte';
-	import Typography from '$lib/components/Typography/Typography.svelte';
-	import FlexColumn from '$lib/components/FlexColumn/FlexColumn.svelte';
-	import ListItem from '$lib/components/ListItem/ListItem.svelte';
-	import VoiceButton from '$lib/components/VoiceButton/VoiceButton.svelte';
-	import CopyButton from '$lib/components/CopyButton/CopyButton.svelte';
 	import ExamQuestionCardHead from '$lib/components/ExamQuestionCardHead/ExamQuestionCardHead.svelte';
+	import ExamQuestionCardBody from '$lib/components/ExamQuestionCardBody/ExamQuestionCardBody.svelte';
 
 	type Props = {
 		question: ExamQuestion;
@@ -19,116 +15,6 @@
 <Card variant="li" borderColor="gray" hasBorderLeft={true}>
 	<Stack size={2} variant="div">
 		<ExamQuestionCardHead {question} />
-		<ul class="options">
-			{#each question.options as option, i}
-				<li class="option" data-is-correct={option.isCorrect}>
-					<FlexColumn
-						gap={1}
-						variant="div"
-						alignItems="center"
-						justifyContent="start"
-						isWrap={true}
-					>
-						<Typography size={2} variant="span" color="primary" weight="bold" align="left">
-							{i + 1}. {option.option}
-						</Typography>
-						<FlexColumn
-							gap={1}
-							variant="div"
-							alignItems="center"
-							justifyContent="start"
-							isWrap={true}
-						>
-							{#if option.option}
-								<VoiceButton text={option.option} />
-								<CopyButton text={option.option} />
-							{/if}
-						</FlexColumn>
-					</FlexColumn>
-					<div class="option-excerpt">
-						{#if option.meaning}
-							<Typography size={2} variant="span" color="dark" weight="normal" align="left">
-								{option.meaning}
-							</Typography>
-						{/if}
-					</div>
-					{#if option.words?.length}
-						<div class="word-list">
-							<Stack size={2} variant="ul">
-								{#each option.words as word}
-									<ListItem symbol="none">
-										<FlexColumn
-											gap={1}
-											variant="div"
-											alignItems="center"
-											justifyContent="start"
-											isWrap={true}
-										>
-											<Typography
-												size={2}
-												variant="span"
-												color="primary"
-												weight="bold"
-												align="left"
-											>
-												{word.word}
-											</Typography>
-											<Typography size={2} variant="span" color="dark" weight="normal" align="left">
-												{word.meaning}
-											</Typography>
-											<FlexColumn
-												gap={1}
-												variant="div"
-												alignItems="center"
-												justifyContent="start"
-												isWrap={true}
-											>
-												{#if word.word}
-													<VoiceButton text={word.word} />
-													<CopyButton text={word.word} />
-												{/if}
-											</FlexColumn>
-										</FlexColumn>
-									</ListItem>
-								{/each}
-							</Stack>
-						</div>
-					{/if}
-				</li>
-			{/each}
-		</ul>
+		<ExamQuestionCardBody {question} />
 	</Stack>
 </Card>
-
-<style>
-	.options {
-		display: grid;
-		list-style: none;
-		padding: 0;
-		margin: 0;
-		border-top: 1px solid var(--color-gray);
-	}
-	@media (max-width: 639px) {
-		.options {
-			row-gap: calc(var(--calc-sp) * var(--spacing-1));
-		}
-	}
-	.option {
-		display: grid;
-		gap: calc(var(--spacing-1) * 1px);
-		padding: calc(var(--spacing-1) * 1px) calc(var(--spacing-2) * 1px);
-		border-bottom: 1px solid var(--color-gray);
-	}
-	.option[data-is-correct='true'] {
-		border-left: 3px solid var(--color-success);
-	}
-	@media (max-width: 639px) {
-		.option {
-			gap: calc(var(--calc-sp) * var(--spacing-1));
-			padding: calc(var(--calc-sp) * var(--spacing-1)) 0;
-		}
-		.option[data-is-correct='true'] {
-			padding-left: calc(var(--calc-sp) * var(--spacing-2));
-		}
-	}
-</style>
--- src/lib/components/ExamQuestionCardHead/ExamQuestionCardHead.svelte
+++ src/lib/components/ExamQuestionCardHead/ExamQuestionCardHead.svelte
@@ -15,51 +15,41 @@
 </script>
 
 <div class="question-block" data-has-words={question.words && question.words.length > 0}>
-	<div class="question-main">
-		<Stack size={1} variant="div">
-			<FlexColumn gap={1} variant="div" alignItems="center" justifyContent="start">
-				<Typography size={4} variant="p" color="primary" weight="bold" align="left">
-					{question.title}
-				</Typography>
-				{#if question.title}
-					<VoiceButton text={question.title} />
-					<CopyButton text={question.title} />
-				{/if}
-			</FlexColumn>
-			<Typography size={2} variant="p" color="dark" weight="normal" align="left">
-				{question.meaning}
+	<Stack size={1} variant="div">
+		<FlexColumn gap={1} variant="div" alignItems="center" justifyContent="start">
+			<Typography size={4} variant="p" color="primary" weight="bold" align="left">
+				{question.title}
 			</Typography>
-		</Stack>
-	</div>
+			{#if question.title}
+				<VoiceButton text={question.title} />
+				<CopyButton text={question.title} />
+			{/if}
+		</FlexColumn>
+		<Typography size={2} variant="p" color="dark" weight="normal" align="left">
+			{question.meaning}
+		</Typography>
+	</Stack>
 	{#if question.words?.length}
-		<div class="word-list footer">
-			<Stack size={2} variant="ul">
-				{#each question.words as word}
-					<ListItem symbol="none">
-						<div class="word-item">
-							<Typography size={2} variant="span" color="primary" weight="bold" align="left">
-								{word.word}
-							</Typography>
-							<Typography size={2} variant="span" color="dark" weight="normal" align="left">
-								{word.meaning}
-							</Typography>
-							<FlexColumn
-								gap={1}
-								variant="div"
-								alignItems="center"
-								justifyContent="start"
-								isWrap={true}
-							>
-								{#if word.word}
-									<VoiceButton text={word.word} />
-									<CopyButton text={word.word} />
-								{/if}
-							</FlexColumn>
-						</div>
-					</ListItem>
-				{/each}
-			</Stack>
-		</div>
+		<Stack size={1} variant="ul">
+			{#each question.words as word}
+				<ListItem symbol="none">
+					<FlexColumn gap={1} variant="div" alignItems="center" justifyContent="start" isWrap>
+						<Typography size={2} variant="span" color="primary" weight="bold" align="left">
+							{word.word}
+						</Typography>
+						<Typography size={2} variant="span" color="dark" weight="normal" align="left">
+							{word.meaning}
+						</Typography>
+						<FlexColumn gap={1} variant="div" alignItems="center" justifyContent="start" isWrap>
+							{#if word.word}
+								<VoiceButton text={word.word} />
+								<CopyButton text={word.word} />
+							{/if}
+						</FlexColumn>
+					</FlexColumn>
+				</ListItem>
+			{/each}
+		</Stack>
 	{/if}
 </div>
 
@@ -67,59 +57,28 @@
 	.question-block {
 		position: relative;
 		display: grid;
-		align-items: start;
 	}
 	@media (min-width: 640px) {
 		.question-block {
-			row-gap: calc(var(--spacing-4) * 1px);
-			column-gap: calc(var(--spacing-4) * 1px);
-		}
-		.question-block[data-has-words='true'] {
-			grid-template-columns: 1fr 1fr;
-		}
-		.question-block[data-has-words='true']::before {
-			content: '';
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			width: 1px;
-			height: 100%;
-			background-color: var(--color-gray);
-			translate: 0 -50%;
+			gap: calc(var(--spacing-4) * 1px);
+			&[data-has-words='true'] {
+				grid-template-columns: 1fr 1fr;
+				&::before {
+					content: '';
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					width: 1px;
+					height: 100%;
+					background-color: var(--color-gray);
+					translate: 0 -50%;
+				}
+			}
 		}
 	}
 	@media (max-width: 639px) {
 		.question-block[data-has-words='true'] {
-			gap: calc(var(--spacing-4) * var(--calc-sp));
-			grid-template-rows: auto 1fr;
-		}
-	}
-	.word-list.footer {
-		position: relative;
-		margin-top: 0;
-		padding-top: 0;
-		border-top: none;
-	}
-	@media (max-width: 639px) {
-		.word-list.footer::before {
-			content: '';
-			position: absolute;
-			top: calc(-1 * var(--spacing-2) * var(--calc-sp));
-			left: 0;
-			width: 100%;
-			height: 1px;
-			background-color: var(--color-gray);
-		}
-	}
-	.word-item {
-		display: flex;
-		flex-wrap: wrap;
-		align-items: center;
-		gap: calc(var(--spacing-4) * 1px);
-	}
-	@media (max-width: 639px) {
-		.word-item {
-			gap: calc(var(--spacing-4) * var(--calc-sp));
+			gap: calc(var(--spacing-2) * var(--calc-sp));
 		}
 	}
 </style>
